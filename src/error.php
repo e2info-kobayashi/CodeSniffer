3a32c2ef37113c888b67e777e83045587ebc282c
*rxiavG3aFp <satoshi_kobayashi@e2info.com>
2025-03-12 11:46:30 +0900

    コードテスト

--- src/error.php
+++ src/error.php
@@ -1,9 +1,18 @@
 <?php
 
+// [副作用] ini設定の変更
+ini_set('upload_max_filesize', '1M');
+
+// [副作用] ファイルの読み込み
+include "component.php";
+
+// [副作用] 出力生成
+echo "<p>";
+
 class exampleClass {
 	public function example_function() {
 		$example_variable = "This is a test.";
-		echo $example_variable;
+		    echo $example_variable;
             echo "tab space.";
 	}
 }
